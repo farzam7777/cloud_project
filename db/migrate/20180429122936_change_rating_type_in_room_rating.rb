959b12f843619e4cc7e61b7dd65e758bdb57a7fa
farzam7777 <l144211@lhr.nu.edu.pk>
2018-04-29 17:46:03 +0500

    heroku push

--- db/migrate/20180429122936_change_rating_type_in_room_rating.rb
+++ db/migrate/20180429122936_change_rating_type_in_room_rating.rb
@@ -1,5 +1,5 @@
 class ChangeRatingTypeInRoomRating < ActiveRecord::Migration[5.1]
   def change
-  	change_column :room_ratings, :rating, :integer
+  	change_column :room_ratings, :rating, 'integer USING CAST(rating AS integer)'
   end
 end
